48af3e7c1464577ba35c61a7b59865e21847399b
Simona Lorenc <lorenc.simona@gmail.com>
2024-02-19 16:39:56 +0100

    rwd

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipes/data/recipe';
 import { RecipesRepository } from '../recipes/data/recipes-repository';
 import { CommonModule } from '@angular/common';
 import { SpacePipe } from '../space.pipe';
 import { RecipesDataCache } from '../recipes/data/recipes-data-cache';
+import { JoinPipe } from '../join.pipe';
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [CommonModule, SpacePipe],
+  imports: [CommonModule, SpacePipe, JoinPipe],
   providers: [Location],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.scss'
 })
 export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe
+  isMobile!: boolean
 
   constructor(private route: Router, private activatedRoute: ActivatedRoute, private recipeRepository: RecipesRepository, private recipesDataCache: RecipesDataCache) {}
 
   ngOnInit(): void {
     this.getRecipe()
+    this.checkScreenWidth()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResixe(event: Event): void {
+    this.checkScreenWidth()
   }
 
   getRecipe() {
@@ -34,5 +42,12 @@ export class RecipeDetailComponent implements OnInit {
         recipe => this.recipe = recipe
       )
     }
+  }
+
+  checkScreenWidth(): boolean {
+    this.isMobile = window.innerWidth <= 992
+    console.log(window.innerWidth)
+    console.log(this.isMobile)
+    return this.isMobile
   }
 }
